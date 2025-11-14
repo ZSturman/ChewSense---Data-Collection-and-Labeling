4e34872eee50baff21d6a04a48aa929d1b67d701
Zachary Sturman <zasturman@gmail.com>
2025-11-13 17:09:38 -0800

    Initial Commit

---
+++ ChewSense_DataCollectionAndLabeling/ChewSense_DataCollectionAndLabelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChewSense_DataCollectionAndLabelingApp.swift
+//  ChewSense_DataCollectionAndLabeling
+//
+//  Created by Zachary Sturman on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ChewSense_DataCollectionAndLabelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChewSense_DataCollectionAndLabelingTests/ChewSense_DataCollectionAndLabelingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChewSense_DataCollectionAndLabelingTests.swift
+//  ChewSense_DataCollectionAndLabelingTests
+//
+//  Created by Zachary Sturman on 11/13/25.
+//
+
+import Testing
+@testable import ChewSense_DataCollectionAndLabeling
+
+struct ChewSense_DataCollectionAndLabelingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
